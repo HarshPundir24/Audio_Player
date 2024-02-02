4a74937ffed82aa08605a36200642f88d7424d92
Harsh Pundir <harshpundir658@gmail.com>
2024-02-02 16:36:27 +0530

    trash button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import AudioPlayer from './components/AudioPlayer';
 import Playlist from './components/Playlist';
 import FileInput from './components/FileInput';
+import LoadingBar from 'react-top-loading-bar';
 
 const App = () => {
   const [audioFiles, setAudioFiles] = useState([]);
   const [currentFileIndex, setCurrentFileIndex] = useState(0);
   const [audioUpload, setAudioUpload] = useState('');
   const [loading, setLoading] = useState(true);
+
+  const loadingBar = useRef(null);
 
   useEffect(() => {
     const storedFiles = JSON.parse(localStorage.getItem('audioFiles')) || [];
@@ -27,17 +30,9 @@ const App = () => {
   const handleFileUpload = async () => {
     if (audioUpload === '') return;
 
-    const data = new FormData();
-    data.append("file", audioUpload);
-    data.append("upload_preset", "akdmxjei");
-    data.append("cloud_name", "djnectplo");
-    data.append("resource_type", "audio");
-
+    loadingBar.current.continuousStart();
     try {
-      const response = await fetch("https://api.cloudinary.com/v1_1/djnectplo/upload", {
-        method: "POST",
-        body: data,
-      });
+      const response = await upload(audioUpload);
 
       if (response.ok) {
         const responseData = await response.json();
@@ -48,10 +43,37 @@ const App = () => {
         setAudioFiles(updatedFiles);
         setAudioUpload('');
       } else {
-        console.error("Failed to upload to Cloudinary");
+        console.error('Failed to upload to Cloudinary');
       }
     } catch (error) {
-      console.error("Error during Cloudinary upload:", error);
+      console.error('Error during Cloudinary upload:', error);
+    } 
+    finally {
+      loadingBar.current.complete();
+    }
+  };
+
+  const upload = async (file) => {
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', 'akdmxjei');
+    data.append('cloud_name', 'djnectplo');
+    data.append('resource_type', 'audio');
+
+    const response = await fetch('https://api.cloudinary.com/v1_1/djnectplo/upload', {
+      method: 'POST',
+      body: data,
+    });
+
+    return response;
+  };
+
+  const removeAudioFile = (index) => {
+    const updatedFiles = [...audioFiles];
+    updatedFiles.splice(index, 1);
+    setAudioFiles(updatedFiles);
+    if (currentFileIndex >= updatedFiles.length) {
+      setCurrentFileIndex(updatedFiles.length - 1);
     }
   };
 
@@ -69,16 +91,22 @@ const App = () => {
 
   return (
     <div className="App mb-5">
-      <h1 className='text-center bg-dark text-white'>Audio Player</h1>
-      <div className='d-flex justify-content-start my-5'>
+      <LoadingBar ref={loadingBar} color="#f11946" />
+      <h1 className="text-center bg-dark text-white">Audio Player</h1>
+      <div className="d-flex justify-content-start my-5">
         <FileInput onFileChange={handleFileChange} />
-        <button className='btn btn-success mt-2 mt-md-0 ml-md-2' onClick={handleFileUpload}>Upload Audio</button>
+        <button className="btn btn-success mt-2 mt-md-0 ml-md-2" onClick={handleFileUpload}>
+          Upload Audio
+        </button>
       </div>
-      <div className='d-flex justify-content-around flex-column-reverse flex-md-row my-5'>
-        <Playlist files={audioFiles} onSelect={handleSelectFile} />
+      <div className="d-flex justify-content-around flex-column-reverse flex-md-row my-5">
+        <Playlist files={audioFiles} onSelect={handleSelectFile} onRemove={removeAudioFile} />
         {audioFiles.length > 0 && (
-          <AudioPlayer  src={audioFiles[currentFileIndex].url}
-            onEnded={handleAudioEnded} songName={audioFiles[currentFileIndex].name} />
+          <AudioPlayer
+            src={audioFiles[currentFileIndex].url}
+            onEnded={handleAudioEnded}
+            songName={audioFiles[currentFileIndex].name}
+          />
         )}
       </div>
     </div>
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const Playlist = ({ files, onSelect }) => {
+const Playlist = ({ files, onSelect, onRemove }) => {
   const [playlistFiles, setPlaylistFiles] = useState([]);
 
   useEffect(() => {
@@ -13,6 +15,14 @@ const Playlist = ({ files, onSelect }) => {
       {playlistFiles.map((file, index) => (
         <li role="button" key={index} onClick={() => onSelect(index)}>
           {file.name}
+          <FontAwesomeIcon
+            icon={faTrash}
+            className='text-danger float-right mx-4'
+            onClick={(e) => {
+              e.stopPropagation();
+              onRemove(index);
+            }}
+          />
         </li>
       ))}
     </ul>
